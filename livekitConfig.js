ac2e099d893dca639e79abd89a76b60848646b9d
LoiyQasrawi <162228572+LoiyQasrawi@users.noreply.github.com>
2024-11-14 16:36:57 -0600

    Add files via upload

---
+++ livekitConfig.js
@@ -0,0 +1,13 @@
+const livekitConfig = {
+  url: "wss://oneshot-8n33h9y0.livekit.cloud/",
+  accessTokens: {
+
+    camera:
+      "eyJhbGciOiJIUzI1NiJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6InF1aWNrc3RhcnQtcm9vbSJ9LCJpc3MiOiJ5b3VyX2FwaV9rZXlfaGVyZSIsImV4cCI6MTczMTU0NzE3OSwibmJmIjowLCJzdWIiOiJxdWlja3N0YXJ0LXVzZXJuYW1lIn0.vvmxqXdQoV262X3wYoafimlQ72vyTmEXVlPIk01CWEs",
+   
+      questPro:
+      "eyJhbGciOiJIUzI1NiJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6InF1aWNrc3RhcnQtcm9vbSJ9LCJpc3MiOiJ5b3VyX2FwaV9rZXlfaGVyZSIsImV4cCI6MTczMTU0NzI3NCwibmJmIjowLCJzdWIiOiJxdWlja3N0YXJ0LXVzZXJuYW1lIn0.Gm_SYlYBMYksmM5dl0VU7M9VjrGGAcBnnBAn-kd0pPw",
+  
+      roomName: "my-first-room",
+  },
+};
