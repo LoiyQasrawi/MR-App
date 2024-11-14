1ff61757755bdb00983533616dcd55dcf2a3e51e
LoiyQasrawi <162228572+LoiyQasrawi@users.noreply.github.com>
2024-11-14 16:41:42 -0600

    Update livekitConfig.js

--- livekitConfig.js
+++ livekitConfig.js
@@ -3,7 +3,7 @@ const livekitConfig = {
   accessTokens: {
 
     camera:
-      "eyJhbGciOiJIUzI1NiJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6InF1aWNrc3RhcnQtcm9vbSJ9LCJpc3MiOiJ5b3VyX2FwaV9rZXlfaGVyZSIsImV4cCI6MTczMTU0NzE3OSwibmJmIjowLCJzdWIiOiJxdWlja3N0YXJ0LXVzZXJuYW1lIn0.vvmxqXdQoV262X3wYoafimlQ72vyTmEXVlPIk01CWEs",
+      "eyJhbGciOiJIUzI1NiJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6InF1aWNrc3RhcnQtcm9vbSJ9LCJpc3MiOiJ5b3VyX2FwaV9rZXlfaGVyZSIsImV4cCI6MTczMTYyNTQ4NCwibmJmIjowLCJzdWIiOiJxdWlja3N0YXJ0LXVzZXJuYW1lIn0.BLCOiUszk-MHsMeXUkZZkYtHZTr11sr6i3AmfdcB2es",
    
       questPro:
       "eyJhbGciOiJIUzI1NiJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6InF1aWNrc3RhcnQtcm9vbSJ9LCJpc3MiOiJ5b3VyX2FwaV9rZXlfaGVyZSIsImV4cCI6MTczMTU0NzI3NCwibmJmIjowLCJzdWIiOiJxdWlja3N0YXJ0LXVzZXJuYW1lIn0.Gm_SYlYBMYksmM5dl0VU7M9VjrGGAcBnnBAn-kd0pPw",
